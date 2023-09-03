4c20760c48c0e48b4d3485ef14c5a9adf2175b2f
Sardius <0xsardius@gmail.com>
2023-09-03 00:09:52 -0400

    Setting up connect wallet btn

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
-import { ConnectWallet, useAddress, useContract } from "@thirdweb-dev/react";
+import { ConnectWallet, useAddress, useContract, useTokenBalance, useOwnedNFTs } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
 import { NextPage } from "next";
+import { PASS_CONTRACT, TOKEN_CONTRACT } from "../constants/addresses";
 
 const Home: NextPage = () => {
   // This hook gets wallet address of connected user
@@ -10,12 +11,42 @@ const Home: NextPage = () => {
   // Check if claimed ERC20 tokens
   const {
     contract: tokenContract
-  } = useContract()
+  } = useContract(TOKEN_CONTRACT);
+
+  const {
+    contract: passContract   
+  } = useContract(PASS_CONTRACT);
+
+  const {
+    data: tokenBalance,
+    isLoading: isTokenBalanceLoading,
+  } = useTokenBalance(tokenContract, address);
+
+  const {
+    data: passBalance,
+    isLoading: isPassBalanceLoading,
+  } = useOwnedNFTs(passContract, address);
+
+
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-
-        
+        <div className={styles.card}>
+          <div className={styles.cardTitle}>
+            <h1>Base app</h1>
+            <ConnectWallet />
+          </div>
+          {!address ? (
+            <p>Please connect your wallet.</p>
+          ) : (
+            !isTokenBalanceLoading ? (
+              tokenBalance && parseInt(ethers.utils.formatUnit(tokenBalance.value, 18)) >= 10  (
+            ) : (
+              <p>Loading...</p>
+            )
+            
+          )}
+        </div>
       </div>
     </main>
   );
